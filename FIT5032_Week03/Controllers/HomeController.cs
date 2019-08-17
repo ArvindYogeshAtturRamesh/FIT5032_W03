8f6602f1c71ca7cc82a692f428f017e87956e12b
Arvind <aatt0002@student.monash.edu>
2019-08-17 23:02:52 +1000

    Committed on Aug 17th

---
+++ FIT5032_Week03/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032_Week03.Models.HelloWorld;
+using FIT5032_Week03.Models.Exercise;
+
+namespace FIT5032_Week03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+
+            //return View();
+            Hello hello = new Hello();
+            ViewBag.Message = hello.GetHello();
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            //ViewBag.Message = "Your contact page.";
+
+            ExampleDictionary exampleDictionary = new ExampleDictionary();
+            exampleDictionary.Example();
+
+            return View();
+            
+
+
+        }
+    }
+}
---
+++ FIT5032_Week03/Models/Exercise/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_Week03.Models.Exercise
+{
+    public class Student
+    {
+        public String Name { get; set; }
+        public String PhoneNumber { get; set; }
+
+        public Student(String name, String phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+    }
+
+    public class ExampleDictionary
+    {
+        public void Example()
+        {
+            Dictionary<int, Student> studentDictionary = new Dictionary<int, Student>();
+            Student studentOne = new Student("Arvind", "0415974649");
+            Student studentTwo = new Student("Sundar", "0412568789");
+            studentDictionary.Add(1, studentOne);
+            studentDictionary.Add(2, studentTwo);
+
+            Student result = new Student("","");
+            studentDictionary.TryGetValue(1, out result);
+
+        }
+    }
+}
